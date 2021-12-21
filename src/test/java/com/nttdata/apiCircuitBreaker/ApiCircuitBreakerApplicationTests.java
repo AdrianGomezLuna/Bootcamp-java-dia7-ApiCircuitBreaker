b61e65c81ad549e32ad52e4b14e8754438c0de17
AdrianGomezLuna <adrigoluna@gmail.com>
2021-12-21 15:20:59 +0100

    first commit

---
+++ src/test/java/com/nttdata/apiCircuitBreaker/ApiCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.apiCircuitBreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
